f9fb6bb60abc123ed52240162c5ecf35d71d5d69
José Loarca <joseloarca97@icloud.com>
2022-03-03 20:49:25 -0600

    Revert "add support for custom user model on admin"
    This reverts commit 88603342c517e64b7677e9ef4245cadbb431c251.

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('SECRET_KEY')
-SECRET_KEY = '-znag)+^=t9fx5n#hxni@f9&ktga7f6t-th634-f(x0y7mu%a1'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
